7e9ae241849d48781439656e00e628f910e8eea6
ivana <miyouqing@126.com>
2018-10-02 09:54:29 -0400

    Added  CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require('gulp');
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-  gutil.log('Workflows are awesome! ');
+gulp.task('coffee', function(){
+  gulp.src('components/coffee/tagline.coffee')
+    .pipe(coffee({ bare: true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+
 });

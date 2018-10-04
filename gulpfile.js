1307a085f84ee5d86a5f72547eb340cb581eff98
ivana <miyouqing@126.com>
2018-10-04 10:10:51 -0400

    Watch as default

--- gulpfile.js
+++ gulpfile.js
@@ -48,4 +48,4 @@ gulp.task('watch', function(){
   gulp.watch('components/sass/*.scss', ['compass']);
 })
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass','watch']);

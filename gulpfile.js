4728ad6df7acb447d468e147e8782bcfa8205612
ivana <miyouqing@126.com>
2018-10-04 11:16:40 -0400

    Added static livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,33 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
-var coffeeSources = 'components/coffee/tagline.coffee';
-
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
-  'components/scripts/pixgrid.js',
   'components/scripts/rclick.js',
-  'components/scripts/template.js',
+  'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
-]
-
+  'components/scripts/template.js'
+];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
-gulp.task('coffee', function(){
+gulp.task('coffee', function() {
   gulp.src(coffeeSources)
-    .pipe(coffee({ bare: true})
-      .on('error', gutil.log))
+    .pipe(coffee({ bare: true })
+    .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
-
 });
 
-gulp.task('js', function(){
+gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -40,12 +41,32 @@ gulp.task('compass', function() {
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
+    .pipe(connect.reload())
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-})
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
+});
 
-gulp.task('default', ['coffee', 'js', 'compass','watch']);
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
